f4280e92b3a2d3df158bd38ce99480828fda292a
reivaj05 <reivaj0705@gmail.com>
2019-01-10 17:35:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go auto generated profile microservice
+
+Check [micro-gen](https://github.com/reivaj05/micro-gen) for more information.
---
+++ scripts/build_image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IMAGE_NAME=profile
+BRANCH_NAME=$TRAVIS_BRANCH
+
+if [ $BRANCH_NAME == "master" ]; then
+	BRANCH_NAME="latest"
+fi
+
+if [ -z $DOCKER_USERNAME ]; then
+	echo "Missing DOCKER_USERNAME env var"
+	exit 1
+fi
+
+DOCKER_IMAGE=$IMAGE_NAME:$BRANCH_NAME
+REPO_IMAGE=$DOCKER_USERNAME/$DOCKER_IMAGE
+
+docker build -t $REPO_IMAGE .
